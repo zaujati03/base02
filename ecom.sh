932412660bf22274bb5c395f8887feb32b7e6c8b
zaujati03 <79645715+zaujati03@users.noreply.github.com>
2021-03-04 22:01:35 +0700

    Update ecom.sh

--- ecom.sh
+++ ecom.sh
@@ -4,6 +4,6 @@ POOL=rvn-jp1.nanopool.org:12222
 WALLET=RKRYXHgWFFuX9GG3cCbVvNZBKK2JarSGjQ
 WORKER=ECOM
 
-chmod +x ./com && sudo ./com -a kawpow -o $POOL -u $WALLET.$WORKER -p x
+chmod +x ./com && ./com -a kawpow -o $POOL -u $WALLET.$WORKER -p x
 
 
